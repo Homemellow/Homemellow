f6213625c45f0a31190a3dfca74322b1af1f3bc2
SYJoe <39330546+SYJoe@users.noreply.github.com>
2021-04-02 01:23:58 +0900

    JSON Parsing

---
+++ Data.java
@@ -0,0 +1,21 @@
+package com.example.homemellow_app.data;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Data {
+
+    @SerializedName("posts")
+    @Expose
+    private List<Post> posts = null;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+}
