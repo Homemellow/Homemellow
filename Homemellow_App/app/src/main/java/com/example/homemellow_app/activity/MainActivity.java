c094f8f10381669bc1f603ac91f986a04835a141
SYJoe <sungyeon52@gmail.com>
2021-04-09 14:20:00 +0900

    MainLayout #2 - Add ActionBar

--- Homemellow_App/app/src/main/java/com/example/homemellow_app/activity/MainActivity.java
+++ Homemellow_App/app/src/main/java/com/example/homemellow_app/activity/MainActivity.java
@@ -1,4 +1,53 @@
 package com.example.homemellow_app.activity;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.homemellow_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btn_search:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.btn_bookmark:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            case R.id.btn_cart:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
